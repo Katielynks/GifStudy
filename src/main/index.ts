847b6de1e52904fa3215081c3691fabaf7a3ea27
bng87 <bng87@students.cs.ubc.ca>
2026-07-02 18:40:44 -0700

    [Feature] Transparency

--- src/main/index.ts
+++ src/main/index.ts
@@ -9,6 +9,7 @@ function createWindow(): void {
     width: 900,
     height: 670,
     frame: false,
+    transparent: true,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
